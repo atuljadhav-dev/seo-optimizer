2a922204c9a2834be54a100120f0912241b9fc05
atuljadhav-dev <atulj9537@gmail.com>
2026-03-10 16:20:00 +0530

    configure mongoose connection and db setup

---
+++ backend/src/config/db.config.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+export const connectDatabase = async (): Promise<void> => {
+    try {
+        const mongoUri = process.env.MONGO_URI;
+
+        if (!mongoUri) {
+            throw new Error(
+                "MONGO_URI environmental variable is missing inside your configuration."
+            );
+        }
+
+        const connection = await mongoose.connect(mongoUri);
+        console.log(
+            `MongoDB Connected Successfully: ${connection.connection.host}`
+        );
+    } catch (error) {
+        console.error(
+            `❌ Database Connection Error: ${(error as Error).message}`
+        );
+        process.exit(1);
+    }
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
 import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+import { connectDatabase } from "./config/db.config.js";
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5002;
 
 app.use(express.json());
 
+connectDatabase();
 app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "Hello from Express + TypeScript!" });
+    res.json({ message: "SEO Optimizer API Backend Running Smoothly." });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`🚀 Server listening dynamically on http://localhost:${PORT}`);
 });
